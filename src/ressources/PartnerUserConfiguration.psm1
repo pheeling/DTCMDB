656df1aff7a085025766e3b1a33d96a6226b7a41
pheeling <philipph@vitalitystream.ch>
2018-04-08 12:15:03 +0200

    Authentication Configuration added

---
+++ src/ressources/PartnerUserConfiguration.psm1
@@ -0,0 +1,28 @@
+class PartnerUserConfiguration 
+{
+    # Instanced Property
+    [Object] $credentialsFile
+    [String] $partnerServiceApiRoot = "https://api.partnercenter.microsoft.com"
+    [String] $authority = "https://login.windows.net/"
+    [String] $userName = $this.credentialsFile.userName
+    [String] $password = $this.credentialsFile.password
+    [String] $resourceUrl = "https://api.partnercenter.microsoft.com"
+    [String] $clientId = "301a8dcb-2861-4831-a4ca-629a59ed6a4f"
+    [String] $applicationDomain = "horgendevelopment.onmicrosoft.com"
+    [String] $commonDomain = "common"
+
+    static [PartnerUserConfiguration] $instance
+
+    static [PartnerUserConfiguration] GetInstance(){
+        if ([PartnerUserConfiguration]::instance -eq $null)
+        {
+            [PartnerUserConfiguration]::instance = [PartnerUserConfiguration]::new()
+        }
+
+        return [PartnerUserConfiguration]::instance
+    }
+
+    setCredentialsFile([String] $credentialsFilePath){
+        $this.credentialsFile =  Import-Clixml $credentialsFilePath;
+    }
+}
