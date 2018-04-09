f022e2416302bee03e9bc9be181b9ba6478fbe2e
pheeling <philipph@vitalitystream.ch>
2018-04-09 23:27:00 +0200

    modules correct, instantion test successful

--- src/main/main.ps1
+++ src/main/main.ps1
@@ -2,4 +2,18 @@ Using Module "..\ressources\PartnerUserConfiguration.psm1"
 
 $configuration = [PartnerUserConfiguration]::new()
 
-$configuration | Get-Member
+# $configuration | Get-Member
+If ($configuration.userName -eq $null){
+    Write-Host "success"
+} else {
+    Write-Host "failed"
+}
+
+$configuration.createLoginCredentialFile()
+$configuration.setCredentialsFile(".\src\ressources\${env:USERNAME}_cred.xml")
+
+If ($configuration.userName -eq $null){
+    Write-Host "failed"
+} else {
+    Write-Host "success"
+}
