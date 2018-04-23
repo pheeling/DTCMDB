b0889f0d078f28f7f8a6a00a484e8dffe19d3953
pheeling <philipph@vitalitystream.ch>
2018-04-23 22:55:24 +0200

    removed comment

--- src/ressources/FreshServiceAssets.psm1
+++ src/ressources/FreshServiceAssets.psm1
@@ -14,9 +14,7 @@ class FreshServiceAssets
         $url = "https://dinotronic.freshservice.com/helpdesk/tickets.json"
         $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:X" -f $this.credentials.GetNetworkCredential().Password)))
         $headers = @{Authorization="Basic $($base64AuthInfo)"}
-        $response = Invoke-RestMethod -Uri $url -Headers $headers -ContentType "application/json" -Method "GET" #-Debug -Verbose #-ErrorAction SilentlyContinue   
-        #$obj += $response.Substring(1) | ConvertFrom-Json
-        #return $obj
+        $response = Invoke-RestMethod -Uri $url -Headers $headers -ContentType "application/json" -Method "GET" #-Debug -Verbose #-ErrorAction SilentlyContinue
         return $response
     }
 }
