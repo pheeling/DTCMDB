41ef7d1387dd04c4d9f7f961ce7a41aba228ce4b
pheeling <philipph@vitalitystream.ch>
2018-04-25 10:15:37 +0200

    added Department get

--- src/ressources/FreshServiceAssets.psm1
+++ src/ressources/FreshServiceAssets.psm1
@@ -17,4 +17,12 @@ class FreshServiceAssets
         $response = Invoke-RestMethod -Uri $url -Headers $headers -ContentType "application/json" -Method "GET" #-Debug -Verbose #-ErrorAction SilentlyContinue
         return $response
     }
+
+    [Object] getFreshServiceDepartments(){
+        $url = "https://dinotronic.freshservice.com/itil/departments.json"
+        $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:X" -f $this.credentials.GetNetworkCredential().Password)))
+        $headers = @{Authorization="Basic $($base64AuthInfo)"}
+        $response = Invoke-RestMethod -Uri $url -Headers $headers -ContentType "application/json" -Method "GET" #-Debug -Verbose #-ErrorAction SilentlyContinue
+        return $response
+    }
 }
